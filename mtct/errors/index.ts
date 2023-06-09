a496be58d7a67b0e59227339d20c64acf455e8d7
Sanjay <ksanjayk@protonmail.com>
2023-06-09 12:52:48 +0530

    custom error handler

---
+++ mtct/errors/index.ts
@@ -0,0 +1,5 @@
+import CustomError from './CustomError';
+import BadRequest from './BadRequest';
+import NotFound from './NotFound';
+import UnAuthenticated from './UnAuthenticated';
+export { CustomError, UnAuthenticated, BadRequest, NotFound };
