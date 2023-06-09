f9c79ee02fa8ab608c653bcd81a84ffd3b51af5d
Sanjay <ksanjayk@protonmail.com>
2023-06-10 00:07:27 +0530

    unauthorized error added

--- mtct/errors/index.ts
+++ mtct/errors/index.ts
@@ -2,4 +2,5 @@ import CustomError from './CustomError';
 import BadRequest from './BadRequest';
 import NotFound from './NotFound';
 import UnAuthenticated from './UnAuthenticated';
-export { CustomError, UnAuthenticated, BadRequest, NotFound };
+import UnAuthorized from './UnAuthorized';
+export { CustomError, UnAuthenticated, BadRequest, NotFound, UnAuthorized };
