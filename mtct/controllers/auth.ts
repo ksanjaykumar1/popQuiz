31454a56e2f654b4cae44bb1aa7357d24c2743e6
Sanjay <ksanjayk@protonmail.com>
2023-06-09 22:58:53 +0530

    login and register

--- mtct/controllers/auth.ts
+++ mtct/controllers/auth.ts
@@ -1,10 +1,38 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
+import * as bcrypt from 'bcryptjs';
+
+import User from '../models/user';
+import { createJWT } from '../utils/jwt';
+import { BadRequest, UnAuthenticated } from '../errors';
+import Logger from '../utils/logger';
 const register = async (req: express.Request, res: express.Response) => {
-  res.status(StatusCodes.CREATED).json({ msg: 'successfully created' });
+  const user = await User.create({ ...req.body });
+  const userToken = { name: user.name, userId: user._id, role: user.role };
+  const token = createJWT({ payload: userToken });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ token, user: { userId: user._id, name: user.name } });
 };
 
 const login = async (req: express.Request, res: express.Response) => {
-  res.status(StatusCodes.OK).json({ msg: 'successfully logged In' });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequest('Provide both email and password');
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new BadRequest("User doesn't exit");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    Logger.error('Password match ==>', isMatch);
+    throw new UnAuthenticated('Invalid Password');
+  }
+  const userToken = { name: user.name, userId: user._id, role: user.role };
+  const token = createJWT({ payload: userToken });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ token, user: { userId: user._id, name: user.name } });
 };
 export { register, login };
