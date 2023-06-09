55b3777cd0e5d0a56c94fdf193419330bd2666f7
Sanjay <ksanjayk@protonmail.com>
2023-06-09 13:01:44 +0530

    different roles and investor status added

---
+++ mtct/utils/enum.ts
@@ -0,0 +1,15 @@
+const ROLES = {
+  HQ_ADMIN: 'hq_admin',
+  BRANCH_ADMIN: 'branch_admin',
+  AGENT: 'agent',
+  INVESTOR: 'investor',
+};
+
+const INVESTOR_STATUS = {
+  ACTIVE: 'active',
+  BLACKLIST: 'blacklist',
+  REJECTED: 'rejected',
+  ONBOARDED: 'onboarded',
+};
+
+export { ROLES, INVESTOR_STATUS };
