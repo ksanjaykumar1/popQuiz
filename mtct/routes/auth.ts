ec691c4765cd9b900959f4d5e339b53f398c584e
Sanjay <ksanjayk@protonmail.com>
2023-06-10 23:50:52 +0530

    route name changed

--- mtct/routes/auth.ts
+++ mtct/routes/auth.ts
@@ -3,7 +3,7 @@ import { login, register } from '../controllers/auth';
 
 const router = express.Router();
 
-router.route('/register').post(register);
+router.route('/sign-up').post(register);
 router.route('/login').post(login);
 
 export default router;
