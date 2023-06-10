c65bf99004acf5f1330215e9d58c0173e8a19705
Sanjay <ksanjayk@protonmail.com>
2023-06-10 23:59:56 +0530

    customer onboarding route added

--- mtct/routes/investor.ts
+++ mtct/routes/investor.ts
@@ -1,5 +1,7 @@
 import express from 'express';
+import { customerOnboarding } from '../controllers/investor';
 
 const router = express.Router();
+router.route('/sign-up').post(customerOnboarding);
 
 export default router;
