ee10e958759c2ecc47d9cb5a0847ab09d1f3cb6b
Sanjay <ksanjayk@protonmail.com>
2023-06-12 04:47:28 +0530

    Aries error handler added

--- weIDY/mtct/errors/index.ts
+++ weIDY/mtct/errors/index.ts
@@ -3,4 +3,12 @@ import BadRequest from './BadRequest';
 import NotFound from './NotFound';
 import UnAuthenticated from './UnAuthenticated';
 import UnAuthorized from './UnAuthorized';
-export { CustomError, UnAuthenticated, BadRequest, NotFound, UnAuthorized };
+import Aries from './Aries';
+export {
+  CustomError,
+  UnAuthenticated,
+  BadRequest,
+  NotFound,
+  UnAuthorized,
+  Aries,
+};
