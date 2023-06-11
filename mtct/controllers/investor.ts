c2f88906699a1b004682ca2fb642aa303f1fa8d2
Sanjay <ksanjayk@protonmail.com>
2023-06-11 13:10:55 +0530

    investor onboarding

--- mtct/controllers/investor.ts
+++ mtct/controllers/investor.ts
@@ -1,36 +1,50 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
-const customerOnboarding = async (
-  req: express.Request,
-  res: express.Response
-) => {
-  res.status(StatusCodes.CREATED).json({ msg: 'created' });
+import User from '../models/User';
+import { BadRequest } from '../errors';
+import Investor from '../models/Investor';
+import Logger from '../utils/logger';
+
+const onboarding = async (req: any, res: express.Response) => {
+  const { email } = req.body;
+
+  //  find if investor has signed up or not
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new BadRequest(`Investor hasn't signed up`);
+  }
+  const investor = await Investor.create({
+    ...req.body,
+    branchId: req.user.branchId,
+    userId: user._id,
+  });
+
+  res.status(StatusCodes.CREATED).json({ msg: 'Investor oboarded ', investor });
 };
 
 const verifyKYC = async (req: express.Request, res: express.Response) => {
   res.status(StatusCodes.CREATED).json({ msg: 'created' });
 };
 
-const getAllCustomerOnboarding = async (
+const getAllOnboarding = async (
   req: express.Request,
   res: express.Response
 ) => {
   res.status(StatusCodes.CREATED).json({ msg: 'created' });
 };
 
-const getAllCustomerOnboardingByBranch = async (
+const getAllOnboardingByBranch = async (
   req: express.Request,
   res: express.Response
 ) => {
   res.status(StatusCodes.CREATED).json({ msg: 'created' });
 };
 
-const getCustomerOnboardingById = async (
+const getOnboardingById = async (
   req: express.Request,
   res: express.Response
 ) => {
   res.status(StatusCodes.CREATED).json({ msg: 'created' });
 };
 
-
-export {}
+export { onboarding };
--- mtct/routes/investor.ts
+++ mtct/routes/investor.ts
@@ -1,7 +1,14 @@
 import express from 'express';
-import { customerOnboarding } from '../controllers/investor';
+import { onboarding } from '../controllers/investor';
+import {
+  authenticateUser,
+  authorizePermissions,
+} from '../middleware/authentication';
+import { ROLES } from '../utils/enum';
 
 const router = express.Router();
-router.route('/sign-up').post(customerOnboarding);
+router
+  .route('/onboard')
+  .post(authenticateUser, authorizePermissions(ROLES.BRANCH_REP), onboarding);
 
 export default router;
