b9c2b1342faac71b259585a30d70b26e29d48a79
Sanjay <ksanjayk@protonmail.com>
2023-06-11 15:27:37 +0530

    submit kyc functionality added

--- mtct/controllers/investorPortal.ts
+++ mtct/controllers/investorPortal.ts
@@ -1,11 +1,24 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
+import KYC from '../models/Kyc';
+
+const submitKyc = async (req: any, res: express.Response) => {
+  const kyc = await KYC.create({
+    ...req.body,
+    userId: req.user.userId,
+    investorId: req.user.investorId,
+  });
+  res.status(StatusCodes.CREATED).json({ msg: 'KYC submitted', kyc });
+};
 
 const myPortfolio = async (req: express.Request, res: express.Response) => {
   res.status(StatusCodes.CREATED).json({ msg: 'created' });
 };
 
-const myTransactionHistory = async (req: express.Request, res: express.Response) => {
+const myTransactionHistory = async (
+  req: express.Request,
+  res: express.Response
+) => {
   res.status(StatusCodes.CREATED).json({ msg: 'created' });
 };
 
@@ -21,14 +34,4 @@ const allSell = async (req: express.Request, res: express.Response) => {
   res.status(StatusCodes.CREATED).json({ msg: 'created' });
 };
 
-const allSaleByBranch = async (req: express.Request, res: express.Response) => {
-  res.status(StatusCodes.CREATED).json({ msg: 'created' });
-};
-
-const allBuyByBranch = async (req: express.Request, res: express.Response) => {
-  res.status(StatusCodes.CREATED).json({ msg: 'created' });
-};
-
-const allSellByBranch = async (req: express.Request, res: express.Response) => {
-  res.status(StatusCodes.CREATED).json({ msg: 'created' });
-};
+export { submitKyc };
--- mtct/routes/investorPortal.ts
+++ mtct/routes/investorPortal.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import { submitKyc } from '../controllers/investorPortal';
+import { authenticateUser } from '../middleware/authentication';
 
 const router = express.Router();
+
+router.route('/submitkyc').post(authenticateUser, submitKyc);
 
 export default router;
