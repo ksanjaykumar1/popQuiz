bf577f9b9c9f30985b9e7a062055a81fb2cbc2bb
Sanjay <ksanjayk@protonmail.com>
2023-06-11 18:17:06 +0530

    assets functionality added

--- mtct/controllers/asset.ts
+++ mtct/controllers/asset.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
-const create = async (req: express.Request, res: express.Response) => {
-  res.status(StatusCodes.CREATED).json({ msg: 'created' });
+import Asset from '../models/Asset';
+
+const create = async (req: any, res: express.Response) => {
+  const asset = await Asset.create({
+    ...req.body,
+    ownerId: req.user.hqAdminId,
+  });
+  res.status(StatusCodes.CREATED).json({ msg: 'asset created', asset });
 };
 
 const updateById = async (req: express.Request, res: express.Response) => {
@@ -9,19 +15,22 @@ const updateById = async (req: express.Request, res: express.Response) => {
 };
 
 const getAll = async (req: express.Request, res: express.Response) => {
-  res.status(StatusCodes.CREATED).json({ msg: 'created' });
+  const assets = await Asset.find();
+  res.status(StatusCodes.CREATED).json({ msg: 'All assets', assets });
 };
 
 const getById = async (req: express.Request, res: express.Response) => {
   res.status(StatusCodes.CREATED).json({ msg: 'created' });
 };
-const getPriceById = async (req: express.Request, res: express.Response) => {
-  res.status(StatusCodes.CREATED).json({ msg: 'created' });
-};
+
 const getAllSold = async (req: express.Request, res: express.Response) => {
-  res.status(StatusCodes.CREATED).json({ msg: 'created' });
+  const assets = await Asset.find({ availableQuantity: { $eq: 0 } });
+  res.status(StatusCodes.CREATED).json({ msg: 'All sold assets', assets });
 };
 
 const getAllForSale = async (req: express.Request, res: express.Response) => {
-  res.status(StatusCodes.CREATED).json({ msg: 'created' });
+  const assets = await Asset.find({ availableQuantity: { $gt: 0 } });
+
+  res.status(StatusCodes.CREATED).json({ msg: 'All For sale assets', assets });
 };
+export { create, getAll, updateById, getById, getAllSold, getAllForSale };
