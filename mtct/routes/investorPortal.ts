5d5b361b9736e6047d4e4b750b1c413da8b035ae
Sanjay <ksanjayk@protonmail.com>
2023-06-12 00:55:28 +0530

    offer acceptance and all sale offer added

--- mtct/routes/investorPortal.ts
+++ mtct/routes/investorPortal.ts
@@ -1,9 +1,11 @@
 import express from 'express';
-import { submitKyc } from '../controllers/investorPortal';
+import { acceptSaleOffer, allSaleOffer, submitKyc } from '../controllers/investorPortal';
 import { authenticateUser } from '../middleware/authentication';
 
 const router = express.Router();
 
-router.route('/submitkyc').post(authenticateUser, submitKyc);
+router.route('/submitkyc').post(submitKyc);
+router.route('/allSaleOffer').get(allSaleOffer);
+router.route('/acceptSaleOffer/:id').post(acceptSaleOffer);
 
 export default router;
